497a919a81604cc6bfb6889ee872ed0f1e5e93fe
George Gelaga-King <gb7309@gmail.com>
2016-08-26 15:34:51 +0000

    Removed Number

--- dhis2-android-app/src/main/java/org/dhis2/mobile/processors/SendSmsProcessor.java
+++ dhis2-android-app/src/main/java/org/dhis2/mobile/processors/SendSmsProcessor.java
@@ -34,7 +34,7 @@ public class SendSmsProcessor {
     public static void send (Context context, DatasetInfoHolder info, ArrayList<Group> groups){
         String data = prepareContent(info, groups, context);
         //insert destination number
-        sendSMS(context, "078000555", data);
+        sendSMS(context, "<insert number here>", data);
         if (!NetworkUtils.checkConnection(context)) {
             saveDataset(context, data, info);
             return;
