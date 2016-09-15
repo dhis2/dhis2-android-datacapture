6d01b328ff8d698e7f133dc6404b89f73ef34ec0
George Gelaga-King <gb7309@gmail.com>
2016-09-15 17:34:47 +0000

    Refactored SMS Processor

--- dhis2-android-app/src/main/java/org/dhis2/mobile/processors/SendSmsProcessor.java
+++ dhis2-android-app/src/main/java/org/dhis2/mobile/processors/SendSmsProcessor.java
@@ -33,92 +33,37 @@ public class SendSmsProcessor {
     public static final String TAG = SendSmsProcessor.class.getSimpleName();
 
     public static void send (Context context, DatasetInfoHolder info, ArrayList<Group> groups){
-        String data = prepareContent(info, groups, context);
+        String data = prepareContent(groups);
         //insert destination number
         sendSMS(context, Constants.SMS_NUMBER, data);
+        //Save dataset for upload when there is internet connection
         if (!NetworkUtils.checkConnection(context)) {
             saveDataset(context, data, info);
             return;
         }
 
-
     }
-    private static String prepareContent(DatasetInfoHolder info, ArrayList<Group> groups, Context context){
-        JsonObject content = new JsonObject();
+    private static String prepareContent(ArrayList<Group> groups){
         KeyGenerator generator = new KeyGenerator();
-        Log.d("Generator", generator.parse(groups, "command"));
-        JsonArray values = putFieldValuesInJson(groups);
-
-        // Retrieve current date
-        LocalDate currentDate = new LocalDate();
-        String completeDate = currentDate.toString(Constants.DATE_FORMAT);
-
-        //Retrieve username
-        String username = PrefUtils.getUserName(context);
-
-        /**
-         Field title names have to be shortened to get more data sent via sms
-         Username = uN
-
-         Oragnisation Unit = oU
-         Dataset = dSet
-         Period = p
-         Complete Date = cD
-         Data Values = dV
-         **/
+        String msg = generator.parse(groups, "command");
 
 
-        content.addProperty("uN", username);
-        content.addProperty("oU", info.getOrgUnitId());
-        content.addProperty("dSet", info.getFormId());
-        content.addProperty("p", info.getPeriod());
-        content.addProperty("cD", completeDate);
-        content.add("dV", values);
-
-        JsonArray categoryOptions = putCategoryOptionsInJson(info.getCategoryOptions());
-        if (categoryOptions != null) {
-            content.add(Constants.ATTRIBUTE_CATEGORY_OPTIONS, categoryOptions);
-        }
-
-        return content.toString();
-    }
-    private static JsonArray putCategoryOptionsInJson(List<CategoryOption> categoryOptions) {
-        if (categoryOptions != null && !categoryOptions.isEmpty()) {
-            JsonArray jsonOptions = new JsonArray();
-
-            // processing category options
-            for (CategoryOption categoryOption : categoryOptions) {
-                jsonOptions.add(categoryOption.getId());
-            }
-
-            return jsonOptions;
-        }
-
-        return null;
+        return msg;
     }
 
-    private static JsonArray putFieldValuesInJson(ArrayList<Group> groups) {
-        JsonArray jFields = new JsonArray();
-        for (Group group : groups) {
-            for (Field field : group.getFields()) {
-                if(!field.getValue().equals("")) {
-                    JsonObject jField = new JsonObject();
-                    jField.addProperty("De", field.getDataElement());
-                    // jField.addProperty(Field.CATEGORY_OPTION_COMBO, field.getCategoryOptionCombo());
-                    jField.addProperty("val", field.getValue());
-                    jFields.add(jField);
-                }
-            }
-        }
-        return jFields;
-    }
-
+    /**
+     * Sends an SMS
+     * @param context Context
+     * @param phoneNumber String The phone number the sms should be sent to.
+     * @param message String The message that should be sent.
+     */
     private static void sendSMS(final Context context, String phoneNumber, String message) {
         SmsManager sms = SmsManager.getDefault();
         ArrayList<String> parts = sms.divideMessage(message);
         sms.sendMultipartTextMessage(phoneNumber, null, parts, null, null);
-        Handler handler = new Handler(Looper.getMainLooper());
 
+        //Display a toast when the sms has been sent. A handler is used so as not to run on the main thread.
+        Handler handler = new Handler(Looper.getMainLooper());
         handler.post(new Runnable() {
 
             @Override
